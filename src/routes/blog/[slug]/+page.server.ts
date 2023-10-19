cad4d0a00ed572408e0b9509d84b998628b8c9cf
Dante Barbieri <code@dantebarbieri.dev>
2023-10-19 10:53:01 -0500

    Add database and programmatically load blog posts (#12)

---
+++ src/routes/blog/[slug]/+page.server.ts
@@ -0,0 +1,2 @@
+import * as db from '$lib/server/database'; import type { PageServerLoad } from './$types';
+export const load: PageServerLoad = async ({ params }) => { return { post: await db.getPost(params.slug), }; };
